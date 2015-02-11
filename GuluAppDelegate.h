c00bdc66f3c2ef513b48b8fae0929c959a42f572
tatsuya1970 <tatsuya.takemura@gmail.com>
2015-02-11 19:03:22 +0900

    gule

---
+++ GuluAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GuluAppDelegate.h
+//  Gulu
+//
+//  Created by 武村 達也 on 13/02/26.
+//  Copyright (c) 2013年 武村 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuluAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gulu
+//
+//  Created by 武村 達也 on 13/02/26.
+//  Copyright (c) 2013年 武村 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GuluAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GuluAppDelegate class]));
+    }
+}
